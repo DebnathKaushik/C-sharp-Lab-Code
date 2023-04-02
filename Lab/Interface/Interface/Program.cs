b72a180a8802db5cbb6ba7e499a6d6e127948b93
DebnathKaushik <debnathkaushik49@gmail.com>
2023-04-02 12:39:25 +0600

    interface

---
+++ Lab/Interface/Interface/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Interface
+{
+    public interface ISound
+    {
+        public void Makesound();
+    }
+
+    public interface IFeed
+    {
+        public void Feed();
+    }
+
+    public class cat : ISound, IFeed
+    {
+        public void Makesound()
+        {
+            Console.WriteLine("Cat make sound");
+        }
+
+        public void Feed()
+        {
+            Console.WriteLine("Cat will eat");
+        }
+    }
+
+    public class Dog: ISound,IFeed
+
+    {
+        public void Feed() 
+        {
+            Console.WriteLine("Dog will eat");
+        }
+        public void Makesound()
+        {
+            Console.WriteLine("Dog make sound");
+        }
+    }
+    class Animal
+    {
+        public static void Main(string[] args) 
+        {
+            ISound [] arr = new ISound[2];
+            arr[0] = new Dog();
+            arr[1] = new cat();
+
+            foreach(ISound i in arr) 
+            {
+                i.Makesound();
+            }
+
+            IFeed[] arr2 = new IFeed[2];
+            arr2[0] = new Dog();
+            arr2[1] = new cat();
+
+            foreach (IFeed i in arr2)
+            {
+                i.Feed();
+            }
+
+
+        }
+    }
+}
