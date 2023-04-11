9efff7ed92ba8fff7ee6be8825aca74e925fc3b0
DebnathKaushik <debnathkaushik49@gmail.com>
2023-04-11 13:34:22 +0600

    calculator

---
+++ Lab/calculator/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.n11 = new System.Windows.Forms.Button();
+            this.n12 = new System.Windows.Forms.Button();
+            this.n13 = new System.Windows.Forms.Button();
+            this.n14 = new System.Windows.Forms.Button();
+            this.n15 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(1, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(801, 62);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // n1
+            // 
+            this.n1.Image = ((System.Drawing.Image)(resources.GetObject("n1.Image")));
+            this.n1.Location = new System.Drawing.Point(12, 80);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(89, 82);
+            this.n1.TabIndex = 1;
+            this.n1.UseVisualStyleBackColor = true;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.Image = ((System.Drawing.Image)(resources.GetObject("n2.Image")));
+            this.n2.Location = new System.Drawing.Point(124, 80);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(89, 82);
+            this.n2.TabIndex = 2;
+            this.n2.UseVisualStyleBackColor = true;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n3
+            // 
+            this.n3.Image = ((System.Drawing.Image)(resources.GetObject("n3.Image")));
+            this.n3.Location = new System.Drawing.Point(234, 80);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(89, 82);
+            this.n3.TabIndex = 3;
+            this.n3.UseVisualStyleBackColor = true;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n4
+            // 
+            this.n4.Image = ((System.Drawing.Image)(resources.GetObject("n4.Image")));
+            this.n4.Location = new System.Drawing.Point(12, 184);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(89, 82);
+            this.n4.TabIndex = 4;
+            this.n4.UseVisualStyleBackColor = true;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.Image = ((System.Drawing.Image)(resources.GetObject("n5.Image")));
+            this.n5.Location = new System.Drawing.Point(124, 184);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(89, 82);
+            this.n5.TabIndex = 5;
+            this.n5.UseVisualStyleBackColor = true;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n6
+            // 
+            this.n6.Image = ((System.Drawing.Image)(resources.GetObject("n6.Image")));
+            this.n6.Location = new System.Drawing.Point(234, 184);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(89, 82);
+            this.n6.TabIndex = 6;
+            this.n6.UseVisualStyleBackColor = true;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // n7
+            // 
+            this.n7.Image = ((System.Drawing.Image)(resources.GetObject("n7.Image")));
+            this.n7.Location = new System.Drawing.Point(12, 280);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(89, 82);
+            this.n7.TabIndex = 7;
+            this.n7.UseVisualStyleBackColor = true;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n8
+            // 
+            this.n8.Image = ((System.Drawing.Image)(resources.GetObject("n8.Image")));
+            this.n8.Location = new System.Drawing.Point(124, 280);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(89, 82);
+            this.n8.TabIndex = 8;
+            this.n8.UseVisualStyleBackColor = true;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n9
+            // 
+            this.n9.Image = ((System.Drawing.Image)(resources.GetObject("n9.Image")));
+            this.n9.Location = new System.Drawing.Point(234, 280);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(89, 82);
+            this.n9.TabIndex = 9;
+            this.n9.UseVisualStyleBackColor = true;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n0
+            // 
+            this.n0.Image = ((System.Drawing.Image)(resources.GetObject("n0.Image")));
+            this.n0.Location = new System.Drawing.Point(124, 368);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(78, 74);
+            this.n0.TabIndex = 10;
+            this.n0.UseVisualStyleBackColor = true;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // n11
+            // 
+            this.n11.Image = ((System.Drawing.Image)(resources.GetObject("n11.Image")));
+            this.n11.Location = new System.Drawing.Point(340, 80);
+            this.n11.Name = "n11";
+            this.n11.Size = new System.Drawing.Size(89, 82);
+            this.n11.TabIndex = 11;
+            this.n11.UseVisualStyleBackColor = true;
+            this.n11.Click += new System.EventHandler(this.n11_Click);
+            // 
+            // n12
+            // 
+            this.n12.Image = ((System.Drawing.Image)(resources.GetObject("n12.Image")));
+            this.n12.Location = new System.Drawing.Point(460, 80);
+            this.n12.Name = "n12";
+            this.n12.Size = new System.Drawing.Size(89, 82);
+            this.n12.TabIndex = 12;
+            this.n12.UseVisualStyleBackColor = true;
+            this.n12.Click += new System.EventHandler(this.n12_Click);
+            // 
+            // n13
+            // 
+            this.n13.Image = ((System.Drawing.Image)(resources.GetObject("n13.Image")));
+            this.n13.Location = new System.Drawing.Point(340, 184);
+            this.n13.Name = "n13";
+            this.n13.Size = new System.Drawing.Size(89, 82);
+            this.n13.TabIndex = 13;
+            this.n13.UseVisualStyleBackColor = true;
+            this.n13.Click += new System.EventHandler(this.n13_Click);
+            // 
+            // n14
+            // 
+            this.n14.Image = ((System.Drawing.Image)(resources.GetObject("n14.Image")));
+            this.n14.Location = new System.Drawing.Point(460, 184);
+            this.n14.Name = "n14";
+            this.n14.Size = new System.Drawing.Size(89, 82);
+            this.n14.TabIndex = 14;
+            this.n14.UseVisualStyleBackColor = true;
+            this.n14.Click += new System.EventHandler(this.n14_Click);
+            // 
+            // n15
+            // 
+            this.n15.Image = ((System.Drawing.Image)(resources.GetObject("n15.Image")));
+            this.n15.Location = new System.Drawing.Point(340, 280);
+            this.n15.Name = "n15";
+            this.n15.Size = new System.Drawing.Size(89, 82);
+            this.n15.TabIndex = 15;
+            this.n15.UseVisualStyleBackColor = true;
+            this.n15.Click += new System.EventHandler(this.n15_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(460, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 82);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n15);
+            this.Controls.Add(this.n14);
+            this.Controls.Add(this.n13);
+            this.Controls.Add(this.n12);
+            this.Controls.Add(this.n11);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button n11;
+        private System.Windows.Forms.Button n12;
+        private System.Windows.Forms.Button n13;
+        private System.Windows.Forms.Button n14;
+        private System.Windows.Forms.Button n15;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
